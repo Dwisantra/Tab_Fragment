69b64d80ce73f2adb3bfcd9928f015a76d8150be
Okvi Dwi Santra <okvidwisantra@gmail.com>
2019-09-16 11:13:03 +0700

    Initial commit

---
+++ app/src/main/java/okvi/dwi/tab_fragment/MainActivity.java
@@ -0,0 +1,14 @@
+package okvi.dwi.tab_fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
